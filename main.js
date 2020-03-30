c9cc0912280e707ddc38f4cbade8dcce6176b6b6
amita125 <amitaghale@gmail.com>
2020-03-30 13:58:35 +0100

    Updated index,style and main file

--- main.js
+++ main.js
@@ -1,55 +1,36 @@
-// let form = document.getElementById("form");
-// let cardSection = document.querySelector("#card-section");
-// let submitButton = document.querySelector("#submitForm");
-// let greetingMessage = document.querySelector(".greeting-h1");
-// let eventName = document.querySelector(".event-name");
-// let messageParagraph = document.querySelector(".message-paragraph");
-
-// document.getElementById("submitForm").addEventListener("click", function(event){
-//     event.preventDefault();
-//     showCard();
-//     });
-
-// function showCard() {
-//     form.style.display= "none";
-//     cardSection.style.display = "block";
-//     greetingMessage.innerHTML = "hello world";
-// };
-
-
-document.addEventListener("DOMContentLoader", function() {
-    console.log("the dom has loaded ");
+document
+  .getElementById("submitForm")
+  .addEventListener("click", function(event) {
+    event.preventDefault();
+    showCard();
   });
-  let form = document.getElementById("form");
-  let cardSection = document.querySelector(".card-section");
-  let submitButton = document.querySelector("#submitForm");
-  let greetingMessage = document.querySelector(".greeting-h1");
-  let eventName = document.querySelector(".event-name");
-  let messageParagraph = document.querySelector(".message-paragraph");
-  document
-    .getElementById("submitForm")
-    .addEventListener("click", function(event) {
-      event.preventDefault();
-      showCard();
-    });
-  function showCard() {
-    form.style.display = "none";
-    cardSection.style.display = "block";
-    var input = document.getElementById("searchTxt");
-    document.getElementById("greetingOK").innerHTML = input.value;
+function showCard() {
+  document.getElementById("form").style.display = "none";
+  document.querySelector(".card-section").style.display = "block";
+  //greeting
+  var msg = document.getElementById("msg");
+  document.getElementById("greetingOK").innerHTML = "Hello   " + msg.value;
+  //event
+  var eventName = document.getElementsByName("eventtype");
+  for (i = 0; i < eventName.length; i++) {
+    if (eventName[i].checked) {
+      document.getElementById("event-name").innerHTML =
+        "You are invited to " + eventName[i].value + " ceremony ..";
+      var occasssion = eventName[i].value;
+
+      if (occasssion == "wedding") {
+        document.querySelector(".card-section").style.backgroundImage =
+          "url('https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/03/24/15/wedding-unsplash-couple.jpg')";
+      } else if (occasssion == "graduation") {
+        document.querySelector(".card-section").style.backgroundImage =
+          "url('https://studybreaks.com/wp-content/uploads/2018/04/Natick-Public-Schools-e1524667439290.jpg')";
+      } else {
+        document.querySelector(".card-section").style.backgroundImage =
+          "url('https://s.yimg.com/ny/api/res/1.2/x6HSYqNr8FjxCVxMIbEByw--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-08/5160dcf0-ba85-11e9-a7dd-8a6639514bb4')";
+      }
+    }
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  //message
+  var msg1 = document.getElementById("message-area").value;
+  document.getElementById("message-paragraph").innerHTML = msg1;
+}
